2889a613fee256bcb18cebffe703b105d1dd831f
LeeSanderson <lee.sanderson@codurance.com>
2026-01-10 16:33:16 +0000

    Set default opinionated rules

--- tests/Opinionated.DotNet.CodingStandards.Tests/UnitTest1.cs
+++ tests/Opinionated.DotNet.CodingStandards.Tests/UnitTest1.cs
@@ -1,11 +1,12 @@
-﻿namespace Opinionated.DotNet.CodingStandards.Tests
-{
-    public class UnitTest1
-    {
-        [Fact]
-        public void Test1()
-        {
+﻿namespace Opinionated.DotNet.CodingStandards.Tests;
 
-        }
+public class UnitTest1
+{
+    private const int I = 1;
+
+    [Fact]
+    public void Test1()
+    {
+        Assert.Equal(1, I);
     }
 }
