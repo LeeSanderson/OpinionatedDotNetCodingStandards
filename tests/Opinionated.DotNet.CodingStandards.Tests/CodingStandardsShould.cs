93d1cc8bd08b70c0d21d216438dc0a31e61771e9
LeeSanderson <lee.sanderson@codurance.com>
2026-01-28 19:29:54 +0000

    Add additional tests on coding standard rules

--- tests/Opinionated.DotNet.CodingStandards.Tests/CodingStandardsShould.cs
+++ tests/Opinionated.DotNet.CodingStandards.Tests/CodingStandardsShould.cs
@@ -4,6 +4,7 @@ using Xunit.Abstractions;
 
 namespace Opinionated.DotNet.CodingStandards.Tests;
 
+
 [Collection(nameof(PackageCollection))]
 public class CodingStandardsShould(PackageFixture fixture, ITestOutputHelper testOutputHelper)
     : CodingStandardsTestBase(fixture, testOutputHelper)
@@ -26,5 +27,49 @@ public class CodingStandardsShould(PackageFixture fixture, ITestOutputHelper tes
         var buildOutput = await project.BuildAndGetOutput();
 
         buildOutput.HasError("IDE0003").ShouldBeFalse();
+    }
+
+    [Fact]
+    public async Task RequireFileScopedNamespaces()
+    {
+        using var project = await CreateProjectBuilder();
+        await project.AddFile(
+            "Program.cs",
+            """
+            namespace test
+            {
+                public static class Program
+                {
+                    public static int Main() => 0;
+                }
+            }
+            """);
+        var buildOutput = await project.BuildAndGetOutput();
+
+        buildOutput.HasError("IDE0161").ShouldBeTrue(); // Convert to file-scoped namespace
+    }
+
+    [Fact]
+    public async Task AllowWarningsAsErrorsToBeDisabled()
+    {
+        using var project = await CreateProjectBuilder(properties: [
+            (Name: "TreatWarningsAsErrors", Value: "false"),
+            (Name: "MSBuildTreatWarningsAsErrors", Value: "false"),
+        ]);
+        await project.AddFile(
+            "Program.cs",
+            """
+            namespace test
+            {
+                public static class Program
+                {
+                    public static int Main() => 0;
+                }
+            }
+            """);
+        var buildOutput = await project.BuildAndGetOutput();
+
+        // Convert to file-scoped namespace should now be a warning instead of an error
+        buildOutput.HasWarning("IDE0161").ShouldBeTrue();
     }
 }
